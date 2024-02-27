217c0b03a9ee2f55fea493f3964eda10bd30efa6
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-27 16:41:18 +0100

    Add Palindrome

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
-
 /**
  * check is a word is palindrome
- * @param {word} word 
+ * @param {string} word 
  * @returns boolean
  */
 function isPalindrome(word) {
-
     // const = function input .split in an array letter per letter .reverse to reverse e .join to return array into string
-    const reverseWord = word.split("").reverse().join("")
+    const reverseWord = word.split("").reverse().join("");
 
     return word === reverseWord;
 }
 
+document.getElementById('check').addEventListener('click', function() {
+    //take word from input
+    let inputWord = document.getElementById('word').value;
 
-document.getElementById('check').addEventListener('click' function(isPalindrome) {
-
-    let inputWord = document.getElementById('word');
-
-    if(true){
-        document.getElementById('result').innerHTML = 'Yes'
+    //check if word is palindrome or not and output
+    if(isPalindrome(inputWord)){
+        document.getElementById('result').innerHTML = 'Yes';
     } else{
-        document.getElementById('result').innerHTML = 'No'  
+        document.getElementById('result').innerHTML = 'No';  
     }
 });
 
